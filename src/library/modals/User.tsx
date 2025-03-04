7b6c17fcdf73f91afd43703116893f54a4381941
Himanshu Chauhan <himanshuchau423@gmail.com>
2025-03-04 17:21:46 +0530

    successfullly created auth backend

---
+++ src/library/modals/User.tsx
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address:{
+    type:String,
+  },
+  otp: {
+    type: String,
+  },
+  isVerified: {
+    type: Boolean,
+  },
+  
+
+});
+
+const User = mongoose.models.Users || mongoose.model("Users", UserSchema);
+
+export default User;
