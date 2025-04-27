a0b8b6d10bd2200e170c48d51f6e0067fbff1952
Himanshu Chauhan <himanshuchau423@gmail.com>
2025-04-27 13:02:38 +0530

    finalized project

--- src/app/api/update-user-data/[email]/route.ts
+++ src/app/api/update-user-data/[email]/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
   req: NextRequest,
-  { params }: { params: Promise<{ email: string }> }
+  { params }: { params: Promise<{ email: string }> }   // Using Promise to ensure params is resolved before use
 ) {
   try {
     await connectToDatabase();
